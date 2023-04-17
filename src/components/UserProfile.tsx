900bf4d27b6d0a277a88e405232874a1139a6711
presentKey <kim.hyeonjae4@gmail.com>
2023-04-17 22:06:58 +0900

    new: 사용자 프로필 구현

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,34 @@
+import { ProfileUser } from '@/model/user';
+import Avatar from './Avatar';
+import FollowButton from './FollowButton';
+
+type Props = {
+  user: ProfileUser;
+};
+
+export default function UserProfile({ user }: Props) {
+  const { image, username, name, followers, following, posts } = user;
+  const info = [
+    { title: 'posts', data: posts },
+    { title: 'followers', data: followers },
+    { title: 'following', data: following },
+  ];
+  return (
+    <section>
+      <Avatar image={image} highlight />
+      <div>
+        <h1>{username}</h1>
+        <FollowButton user={user} />
+        <ul>
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span>{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p>{name}</p>
+      </div>
+    </section>
+  );
+}
