6ad2c0167684f83a7ec4aad7f5135470b3864fe7
presentKey <kim.hyeonjae4@gmail.com>
2023-04-17 23:08:53 +0900

    new: 사용자 포스트 컴포넌트 구현

--- src/components/userPosts.tsx
+++ src/components/userPosts.tsx
@@ -2,16 +2,35 @@
 
 import { ProfileUser } from '@/model/user';
 import { useState } from 'react';
-import useSWR from 'swr';
+import PostIcon from './ui/icons/PostIcon';
+import BookmarkIcon from './ui/icons/BookmarkIcon';
+import HeartIcon from './ui/icons/HeartIcon';
+import PostGrid from './PostGrid';
 
 type Props = {
   user: ProfileUser;
 };
 
-export default function UserPosts({ user: { username } }: Props) {
-  const [tab, setTab] = useState('posts');
-  const { data: posts, isLoading, error } = useSWR(`/api/users/${username}/${tab}`);
-  console.log(posts);
+const tabs = [
+  { type: 'posts', icon: <PostIcon /> },
+  { type: 'saved', icon: <BookmarkIcon className='w-3 h-3' /> },
+  { type: 'liked', icon: <HeartIcon className='w-3 h-3' /> },
+];
 
-  return <div></div>;
+export default function UserPosts({ user: { username } }: Props) {
+  const [query, setQuery] = useState(tabs[0].type);
+
+  return (
+    <section>
+      <ul>
+        {tabs.map(({ type, icon }) => (
+          <li key={type} onClick={() => setQuery(type)}>
+            <button>{icon}</button>
+            <span>{type}</span>
+          </li>
+        ))}
+      </ul>
+      <PostGrid username={username} query={query} />
+    </section>
+  );
 }
